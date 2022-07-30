9707243c3b2fc101badb160870dd7fb76f1b3b90
johnsey <johnsey.reynolds@gmail.com>
2022-07-30 17:48:36 -0400

    Assignment Completed

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,15 @@
-require 'pry'
+# require 'pry'
 
 class Artist
   attr_accessor :name
   attr_reader :songs
 
-  @@artists = []
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
+    @@artists = []
 
   def initialize
     @@artists << self
@@ -19,24 +24,16 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
   def add_song(song)
     @songs << song
-    song.artist = self
+    song.artist = self unless song.artist
   end
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
